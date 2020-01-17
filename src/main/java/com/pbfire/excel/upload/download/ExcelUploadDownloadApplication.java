5a40d0e79e68b65a08d5c61e1778469e5f48ab3a
liuting <1192708515>
2020-01-17 11:30:08 +0800

    新建上传下载excel项目

---
+++ src/main/java/com/pbfire/excel/upload/download/ExcelUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.pbfire.excel.upload.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbfire/excel/upload/download/ExcelUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbfire.excel.upload.download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
